6cfdc9ddefe21b5bbf82a10aa7e64d58360cc700
Alexander Shyshkov <shisha_90@list.ru>
2019-01-16 23:06:20 +0200

    Complete

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ $('#tabs li').click(function(){
 });
 $('li.t1').click();
 });
+
+$(document).ready(function(){
+$('#tabs-mobile li').click(function(){
+	var thisClass = this.className.slice(0,2);
+	$('div.t1').hide();
+	$('div.t2').hide();
+	$('div.t3').hide();
+	$('div.' + thisClass).fadeIn(500);
+	$('#tabs-mobile li').removeClass('active');
+	$(this).addClass('active');
+	return false;
+});
+$('li.t1').click();
+});
